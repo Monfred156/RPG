cdc3f9919dc0ae1eb4b3112fae065317779c4734
William <william.petitprez@epitech.eu>
2019-05-04 14:08:25 +0200

    [~] fix every hitbox from tuto

--- src/initia/initia_tuto.c
+++ src/initia/initia_tuto.c
@@ -32,47 +32,47 @@ void initia_sprite_tuto(global *gb)
 
 void initia_hitbox_tuto2(global *gb)
 {
-    gb->hitbox[9] = *initia_hitbox(&gb->hitbox[9], (sfVector2f) {50, 490},
+    gb->hitbox[9] = *initia_hitbox(&gb->hitbox[9], (sfVector2f) {10, 480},
+        (sfVector2f) {1800, 200});
+    gb->hitbox[10] = *initia_hitbox(&gb->hitbox[10], (sfVector2f) {10, 480},
+        (sfVector2f) {2227, 200});
+    gb->hitbox[11] = *initia_hitbox(&gb->hitbox[11], (sfVector2f) {427, 10},
         (sfVector2f) {1810, 200});
-    gb->hitbox[10] = *initia_hitbox(&gb->hitbox[10], (sfVector2f) {50, 490},
-        (sfVector2f) {2200, 200});
-    gb->hitbox[11] = *initia_hitbox(&gb->hitbox[11], (sfVector2f) {440, 30},
-        (sfVector2f) {1810, 200});
-    gb->hitbox[12] = *initia_hitbox(&gb->hitbox[12], (sfVector2f) {250, 100},
-        (sfVector2f) {600, 250});
-    gb->hitbox[13] = *initia_hitbox(&gb->hitbox[13], (sfVector2f) {130, 70},
-        (sfVector2f) {1380, 700});
-    gb->hitbox[14] = *initia_hitbox(&gb->hitbox[14], (sfVector2f) {90, 50},
-        (sfVector2f) {1645, 700});
-    gb->hitbox[15] = *initia_hitbox(&gb->hitbox[15], (sfVector2f) {90, 50},
-        (sfVector2f) {1865, 700});
-    gb->hitbox[16] = *initia_hitbox(&gb->hitbox[16], (sfVector2f) {90, 50},
-        (sfVector2f) {2100, 700});
-    gb->hitbox[17] = *initia_hitbox(&gb->hitbox[17], (sfVector2f) {90, 50},
-        (sfVector2f) {2340, 700});
-    gb->hitbox[18] = *initia_hitbox(&gb->hitbox[18], (sfVector2f) {90, 80},
-        (sfVector2f) {1985, 250});
+    gb->hitbox[12] = *initia_hitbox(&gb->hitbox[12], (sfVector2f) {200, 10},
+        (sfVector2f) {600, 310});
+    gb->hitbox[13] = *initia_hitbox(&gb->hitbox[13], (sfVector2f) {70, 70},
+        (sfVector2f) {1410, 700});
+    gb->hitbox[14] = *initia_hitbox(&gb->hitbox[14], (sfVector2f) {20, 10},
+        (sfVector2f) {1670, 720});
+    gb->hitbox[15] = *initia_hitbox(&gb->hitbox[15], (sfVector2f) {20, 10},
+        (sfVector2f) {1890, 720});
+    gb->hitbox[16] = *initia_hitbox(&gb->hitbox[16], (sfVector2f) {20, 10},
+        (sfVector2f) {2127, 720});
+    gb->hitbox[17] = *initia_hitbox(&gb->hitbox[17], (sfVector2f) {20, 10},
+        (sfVector2f) {2370, 720});
+    gb->hitbox[18] = *initia_hitbox(&gb->hitbox[18], (sfVector2f) {10, 10},
+        (sfVector2f) {2016, 270});
 }
 
 void initia_hitbox_tuto(global *gb)
 {
-    gb->hitbox[0] = *initia_hitbox(&gb->hitbox[0], (sfVector2f) {10, 540},
+    gb->hitbox[0] = *initia_hitbox(&gb->hitbox[0], (sfVector2f) {10, 480},
         (sfVector2f) {510, 200});
-    gb->hitbox[1] = *initia_hitbox(&gb->hitbox[1], (sfVector2f) {1000, 10},
+    gb->hitbox[1] = *initia_hitbox(&gb->hitbox[1], (sfVector2f) {1010, 10},
         (sfVector2f) {515, 200});
-    gb->hitbox[2] = *initia_hitbox(&gb->hitbox[2], (sfVector2f) {10, 569},
+    gb->hitbox[2] = *initia_hitbox(&gb->hitbox[2], (sfVector2f) {10, 486},
         (sfVector2f) {1520, 200});
-    gb->hitbox[3] = *initia_hitbox(&gb->hitbox[3], (sfVector2f) {635, 20},
+    gb->hitbox[3] = *initia_hitbox(&gb->hitbox[3], (sfVector2f) {586, 10},
         (sfVector2f) {510, 678});
-    gb->hitbox[4] = *initia_hitbox(&gb->hitbox[4], (sfVector2f) {660, 30},
-        (sfVector2f) {1315, 665});
-    gb->hitbox[5] = *initia_hitbox(&gb->hitbox[5], (sfVector2f) {50, 500},
-        (sfVector2f) {910, 750});
-    gb->hitbox[6] = *initia_hitbox(&gb->hitbox[6], (sfVector2f) {1600, 30},
-        (sfVector2f) {910, 1130});
-    gb->hitbox[7] = *initia_hitbox(&gb->hitbox[7], (sfVector2f) {50, 500},
-        (sfVector2f) {2455, 660});
-    gb->hitbox[8] = *initia_hitbox(&gb->hitbox[8], (sfVector2f) {400, 30},
-        (sfVector2f) {2090, 665});
+    gb->hitbox[4] = *initia_hitbox(&gb->hitbox[4], (sfVector2f) {586, 10},
+        (sfVector2f) {1343, 678});
+    gb->hitbox[5] = *initia_hitbox(&gb->hitbox[5], (sfVector2f) {10, 473},
+        (sfVector2f) {900, 680});
+    gb->hitbox[6] = *initia_hitbox(&gb->hitbox[6], (sfVector2f) {1580, 10},
+        (sfVector2f) {900, 1143});
+    gb->hitbox[7] = *initia_hitbox(&gb->hitbox[7], (sfVector2f) {10, 475},
+        (sfVector2f) {2480, 678});
+    gb->hitbox[8] = *initia_hitbox(&gb->hitbox[8], (sfVector2f) {373, 10},
+        (sfVector2f) {2116, 678});
     initia_hitbox_tuto2(gb);
 }
