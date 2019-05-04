97572f325c3f155e5959955b6da50ecc43d54659
Bouhin Christophe <chbouhin@localhost.localdomain>
2019-05-04 19:10:57 +0200

    change movement mob

--- src/mob/movement_mob.c
+++ src/mob/movement_mob.c
@@ -13,6 +13,7 @@ void movement_top_mob(global *gb, float time_sec, int mob, int animation)
 {
     int val[2] = {VAL_MIN_TUTO, VAL_MAX_TUTO};
 
+    gb->mob[mob].time += gb->clock.seconds - gb->clock.save_sec;
     if (mob != 0) {
         val[0] = VAL_MIN_ASH;
         val[1] = VAL_MAX_ASH;
@@ -20,8 +21,8 @@ void movement_top_mob(global *gb, float time_sec, int mob, int animation)
     if (check_hitbox_up(gb, gb->mob[mob].sprite, time_sec, val) == 0) {
         gb->mob[mob].pos.y -= time_sec * 0.9;
         gb->move[mob + 1].movement = ANIM_TOP;
-        if (gb->mob[mob].time + 0.1 < gb->clock.seconds) {
-            gb->mob[mob].time = gb->clock.seconds;
+        if (gb->mob[mob].time > 0.1) {
+            gb->mob[mob].time = 0;
             gb->move[mob + 1].walk += animation;
         }
     }
@@ -31,6 +32,7 @@ void movement_back_mob(global *gb, float time_sec, int mob, int animation)
 {
     int val[2] = {VAL_MIN_TUTO, VAL_MAX_TUTO};
 
+    gb->mob[mob].time += gb->clock.seconds - gb->clock.save_sec;
     if (mob != 0) {
         val[0] = VAL_MIN_ASH;
         val[1] = VAL_MAX_ASH;
@@ -38,8 +40,8 @@ void movement_back_mob(global *gb, float time_sec, int mob, int animation)
     if (check_hitbox_down(gb, gb->mob[mob].sprite, time_sec, val) == 0) {
         gb->mob[mob].pos.y += time_sec * 0.9;
         gb->move[mob + 1].movement = ANIM_BACK;
-        if (gb->mob[mob].time + 0.1 < gb->clock.seconds) {
-            gb->mob[mob].time = gb->clock.seconds;
+        if (gb->mob[mob].time > 0.1) {
+            gb->mob[mob].time = 0;
             gb->move[mob + 1].walk += animation;
         }
     }
@@ -49,6 +51,7 @@ void movement_left_mob(global *gb, float time_sec, int mob)
 {
     int val[2] = {VAL_MIN_TUTO, VAL_MAX_TUTO};
 
+    gb->mob[mob].time += gb->clock.seconds - gb->clock.save_sec;
     if (mob != 0) {
         val[0] = VAL_MIN_ASH;
         val[1] = VAL_MAX_ASH;
@@ -56,8 +59,8 @@ void movement_left_mob(global *gb, float time_sec, int mob)
     if (check_hitbox_left(gb, gb->mob[mob].sprite, time_sec, val) == 0) {
         gb->mob[mob].pos.x -= time_sec;
         gb->move[mob + 1].movement = ANIM_LEFT;
-        if (gb->mob[mob].time + 0.1 < gb->clock.seconds) {
-            gb->mob[mob].time = gb->clock.seconds;
+        if (gb->mob[mob].time > 0.1) {
+            gb->mob[mob].time = 0;
             gb->move[mob + 1].walk += 1;
         }
     }
@@ -67,6 +70,7 @@ void movement_right_mob(global *gb, float time_sec, int mob)
 {
     int val[2] = {VAL_MIN_TUTO, VAL_MAX_TUTO};
 
+    gb->mob[mob].time += gb->clock.seconds - gb->clock.save_sec;
     if (mob != 0) {
         val[0] = VAL_MIN_ASH;
         val[1] = VAL_MAX_ASH;
@@ -74,8 +78,8 @@ void movement_right_mob(global *gb, float time_sec, int mob)
     if (check_hitbox_right(gb, gb->mob[mob].sprite, time_sec, val) == 0) {
         gb->mob[mob].pos.x += time_sec;
         gb->move[mob + 1].movement = ANIM_RIGHT;
-        if (gb->mob[mob].time + 0.1 < gb->clock.seconds) {
-            gb->mob[mob].time = gb->clock.seconds;
+        if (gb->mob[mob].time > 0.1) {
+            gb->mob[mob].time = 0;
             gb->move[mob + 1].walk += 1;
         }
     }
