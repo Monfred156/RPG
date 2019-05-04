75d84286284e5ce8d4f91401680190396847b666
Monfred <alexandre.malaquin@epitech.eu>
2019-05-04 19:56:30 +0200

    [+] src/check_2_in.c : Function can determine if a Sprite is in a RectShape

---
+++ src/check_2_in.c
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2018
+** tavern
+** File description:
+** tavern
+*/
+
+#include "function.h"
+
+int collision_between__sprite(sfSprite *sprite1, sfRectangleShape *rect)
+{
+    sfFloatRect f_rect = sfSprite_getGlobalBounds(sprite1);
+    sfFloatRect f_rect1 = sfRectangleShape_getGlobalBounds(rect);
+    sfFloatRect inter;
+
+    if (sfFloatRect_intersects(&f_rect, &f_rect1, &inter))
+        return 1;
+    else
+        return 0;
+}
