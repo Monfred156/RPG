a7dc00e6a4c294cb61e22723daec5f9b42524ced
alfred.deschildre <alfred.deschildre@epitech.eu>
2019-05-05 21:41:25 +0200

    fix buble for texte

--- src/initia/initia_speak.c
+++ src/initia/initia_speak.c
@@ -13,7 +13,7 @@ void initia_sprite_speak(global *gb)
 {
     gb->sprite[BACK_TEXT] = *initia_sprite(&gb->sprite[BACK_TEXT],
         "./assets/graphics/bulle.jpg",
-        (sfVector2f) {1000, 730}, (sfIntRect) {0, 0, 839, 297});
+        (sfVector2f) {980, 690}, (sfIntRect) {0, 0, 839, 297});
 }
 
 void initia_dialogue(global *gb)
@@ -23,11 +23,11 @@ void initia_dialogue(global *gb)
                      " ce monstre!\nFais attention a toi mon petit..."
                      , "./assets/font/arial.ttf", NULL};
 
-    initia_text(&gb->text[PNJ_TUTO], send, (sfVector2f){1020, 750}, sfBlack, 40);
+    initia_text(&gb->text[PNJ_TUTO], send, (sfVector2f){1000, 700}, sfBlack, 40);
     send[0] = my_strdup("Avant j'etais aventurier.. Mais je me suis\n pris"
                         "une fleche dans le genoux..\n\n"
                         "[+] Peux-tu tuer 2 orcs au nom d'Ulfric?");
-    initia_text(&gb->text[PNJ_ULFRIC], send, (sfVector2f){1020, 750}, sfBlack, 40);
-    send[0] = my_strdup("Salut <3");
-    initia_text(&gb->text[PNJ_LIFE], send, (sfVector2f){1020, 750}, sfBlack, 40);
+    initia_text(&gb->text[PNJ_ULFRIC], send, (sfVector2f){1000, 700}, sfBlack, 40);
+    send[0] = my_strdup("Rien ne vaut une chope d'Hydromel\npour repartir de bons pieds ! <3");
+    initia_text(&gb->text[PNJ_LIFE], send, (sfVector2f){1000, 700}, sfBlack, 40);
 }
